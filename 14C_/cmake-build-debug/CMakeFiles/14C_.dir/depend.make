3fd972487b0af9e74713e6fb766a529bd6d369c0
unknown <471151015@qq.com>
2019-03-04 20:38:49 +0800

    delete3/4

--- 14C_/cmake-build-debug/CMakeFiles/14C_.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for 14C_.
-# This may be replaced when dependencies are built.
--- 18C_/func.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-#define N 11
-
-void arr_insert(int*);
